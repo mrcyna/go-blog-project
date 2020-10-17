c9f4819ad2134c96e1d80c40d6a8ddd522c23d19
Sina Ahmadpour <sina.ahmadpour@zoodfood.com>
2020-10-17 20:46:40 +0330

    root command has been updated

--- cmd/root.go
+++ cmd/root.go
@@ -15,15 +15,12 @@ var (
 	userLicense string
 
 	rootCmd = &cobra.Command{
-		Use:   "blogcrud",
-		Short: "A generator for Cobra based Applications",
-		Long: `Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Use:   "blog",
+		Short: "Blog app",
+		Long:  "Blah Blah Blah About The **BLOG APP**",
 	}
 )
 
-// Execute executes the root command.
 func Execute() error {
 	return rootCmd.Execute()
 }
